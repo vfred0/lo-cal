d02019cad1016335d2176a7ea9cf1c8fd092ace6
Víctor Arreaga <arreagav17@gmail.com>
2025-03-22 20:41:53 -0500

    chore: refactor

--- src/contexts/Preferences.tsx
+++ src/contexts/Preferences.tsx
@@ -1,187 +1,147 @@
-import {
-  createContext,
-  useContext,
-  useState,
-  useEffect,
-  PropsWithChildren,
-} from "react";
-import { noop } from "@/shared/utils";
+import {createContext, PropsWithChildren, useContext, useEffect, useState,} from "react";
+import {noop} from "@/shared/utils";
 
 export type ViewMode = "day" | "week" | "month";
 
 export type PreferenceMap = {
-  "show-welcome-message": boolean;
-  "base-color": string;
-  "view-mode": ViewMode;
-  timezone: string;
-  "last-viewed-date": string;
-  "first-day-of-week": 0 | 1 | 2 | 3 | 4 | 5 | 6;
-  "show-time": boolean;
-  "date-time-format-locale": string;
-  "date-time-format": Pick<
-    Intl.DateTimeFormatOptions,
-    "dateStyle" | "timeStyle"
-  >;
+    "show-welcome-message": boolean;
+    "base-color": string;
+    "view-mode": ViewMode;
+    timezone: string;
+    "last-viewed-date": string;
+    "first-day-of-week": 0 | 1 | 2 | 3 | 4 | 5 | 6;
+    "show-time": boolean;
+    "date-time-format-locale": string;
+    "date-time-format": Pick<
+        Intl.DateTimeFormatOptions,
+        "dateStyle" | "timeStyle"
+    >;
 };
 
 export const DEFAULT_PREFERENCES: PreferenceMap = {
-  "show-welcome-message": true,
-  "base-color": "white",
-  "view-mode": "month",
-  timezone: Intl.DateTimeFormat().resolvedOptions().timeZone,
-  "last-viewed-date": new Date().toISOString(),
-  "first-day-of-week": 1,
-  "show-time": false,
-  "date-time-format-locale": navigator.language,
-  "date-time-format": {
-    dateStyle: "medium",
-    timeStyle: "short",
-  },
+    "show-welcome-message": false,
+    "base-color": "black",
+    "view-mode": "month",
+    timezone: Intl.DateTimeFormat().resolvedOptions().timeZone,
+    "last-viewed-date": new Date().toISOString(),
+    "first-day-of-week": 1,
+    "show-time": false,
+    "date-time-format-locale": 'es-ES',
+    "date-time-format": {
+        dateStyle: "medium",
+        timeStyle: "short",
+    },
 };
-
-const LOCAL_STORAGE_KEY = "preferences";
 
 export type PreferenceKey = keyof PreferenceMap;
 
 type SetPreference = <const K extends PreferenceKey>(
-  key: K,
-  value: PreferenceMap[K]
+    key: K,
+    value: PreferenceMap[K]
 ) => void;
 
 type SetSpecificPreference<K extends PreferenceKey> = (
-  value: PreferenceMap[K]
+    value: PreferenceMap[K]
 ) => void;
 
 type PreferencesContextType = {
-  preferences: PreferenceMap;
-  setPreference: SetPreference;
-  setPreferences: (preferences: PreferenceMap) => void;
-  resetPreference: (key: PreferenceKey) => void;
-  resetPreferences: () => void;
+    preferences: PreferenceMap;
+    setPreference: SetPreference;
+    setPreferences: (preferences: PreferenceMap) => void;
+    resetPreference: (key: PreferenceKey) => void;
+    resetPreferences: () => void;
 };
 
 const PreferencesContext = createContext<PreferencesContextType>({
-  preferences: DEFAULT_PREFERENCES,
-  setPreference: noop,
-  setPreferences: noop,
-  resetPreference: noop,
-  resetPreferences: noop,
+    preferences: DEFAULT_PREFERENCES,
+    setPreference: noop,
+    setPreferences: noop,
+    resetPreference: noop,
+    resetPreferences: noop,
 });
 
 export function usePreferences(): PreferencesContextType {
-  return useContext(PreferencesContext);
+    return useContext(PreferencesContext);
 }
 
 export function usePreference<const K extends PreferenceKey>(
-  key: K
+    key: K
 ): [PreferenceMap[K], SetSpecificPreference<K>] {
-  const { preferences, setPreference } = usePreferences();
-  return [preferences[key], (value) => setPreference(key, value)];
-}
-
-function getLocalPreferences(override?: Partial<PreferenceMap>): PreferenceMap {
-  const storedPreferences = JSON.parse(
-    localStorage.getItem(LOCAL_STORAGE_KEY) || "{}"
-  );
-
-  return {
-    ...DEFAULT_PREFERENCES,
-    ...storedPreferences,
-    ...override,
-  };
-}
-
-function setLocalPreference<const K extends PreferenceKey>(
-  key: K,
-  value: PreferenceMap[K]
-): PreferenceMap {
-  const newPreferences = getLocalPreferences({ [key]: value });
-
-  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(newPreferences));
-  return newPreferences;
-}
-
-function setLocalPreferences(preferences: PreferenceMap): PreferenceMap {
-  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(preferences));
-  return preferences;
-}
-
-function resetLocalPreference(key: PreferenceKey): PreferenceMap {
-  return setLocalPreference(key, DEFAULT_PREFERENCES[key]);
-}
-
-function resetLocalPreferences(): PreferenceMap {
-  return setLocalPreferences(DEFAULT_PREFERENCES);
+    const {preferences, setPreference} = usePreferences();
+    return [preferences[key], (value) => setPreference(key, value)];
 }
 
 function setBaseColor(color: string): void {
-  document.documentElement.style.setProperty("--base-color", color);
+    document.documentElement.style.setProperty("--base-color", color);
 
-  const themeColorReference = document.getElementById("theme-color-reference");
+    const themeColorReference = document.getElementById("theme-color-reference");
 
-  if (!themeColorReference) {
-    return;
-  }
+    if (!themeColorReference) {
+        return;
+    }
 
-  const themeColor =
-    getComputedStyle(themeColorReference).getPropertyValue("background-color");
+    const themeColor =
+        getComputedStyle(themeColorReference).getPropertyValue("background-color");
 
-  document.head
-    .querySelector(`meta[name="theme-color"]`)
-    ?.setAttribute("content", themeColor);
+    document.head
+        .querySelector(`meta[name="theme-color"]`)
+        ?.setAttribute("content", themeColor);
 }
 
-export default function PreferencesProvider({ children }: PropsWithChildren) {
-  const [preferences, setPreferences] = useState(() => {
-    return getLocalPreferences();
-  });
+export default function PreferencesProvider({children}: PropsWithChildren) {
+    const [preferences, setPreferences] = useState<PreferenceMap>(DEFAULT_PREFERENCES);
 
-  const setPreference: SetPreference = (key, value) => {
-    setPreferences(() => {
-      if (key === "base-color") {
-        setBaseColor(value as string);
-      }
+    const setPreference: SetPreference = (key, value) => {
+        setPreferences((prevPreferences) => {
+            const newPreferences = {...prevPreferences, [key]: value};
 
-      return setLocalPreference(key, value);
-    });
-  };
+            if (key === "base-color") {
+                setBaseColor(value as string);
+            }
 
-  const setContextPreferences = (preferences: PreferenceMap) => {
-    setPreferences(() => {
-      setBaseColor(preferences["base-color"]);
-      return setLocalPreferences(preferences);
-    });
-  };
+            return newPreferences;
+        });
+    };
 
-  const resetPreference = (key: PreferenceKey) => {
-    setPreferences(() => {
-      return resetLocalPreference(key);
-    });
-  };
+    const setContextPreferences = (newPreferences: PreferenceMap) => {
+        setPreferences(() => {
+            setBaseColor(newPreferences["base-color"]);
+            return newPreferences;
+        });
+    };
 
-  const resetPreferences = () => {
-    setPreferences(() => {
-      setBaseColor(DEFAULT_PREFERENCES["base-color"]);
-      return resetLocalPreferences();
-    });
-  };
+    const resetPreference = (key: PreferenceKey) => {
+        setPreferences((prevPreferences) => ({
+            ...prevPreferences,
+            [key]: DEFAULT_PREFERENCES[key]
+        }));
 
-  useEffect(() => {
-    setBaseColor(preferences["base-color"]);
-    /* eslint-disable-next-line react-hooks/exhaustive-deps */
-  }, []);
+        if (key === "base-color") {
+            setBaseColor(DEFAULT_PREFERENCES["base-color"]);
+        }
+    };
 
-  return (
-    <PreferencesContext.Provider
-      value={{
-        preferences,
-        setPreference,
-        resetPreference,
-        resetPreferences,
-        setPreferences: setContextPreferences,
-      }}
-    >
-      {children}
-    </PreferencesContext.Provider>
-  );
+    const resetPreferences = () => {
+        setPreferences(DEFAULT_PREFERENCES);
+        setBaseColor(DEFAULT_PREFERENCES["base-color"]);
+    };
+
+    useEffect(() => {
+        setBaseColor(preferences["base-color"]);
+        /* eslint-disable-next-line react-hooks/exhaustive-deps */
+    }, []);
+
+    return (
+        <PreferencesContext.Provider
+            value={{
+                preferences,
+                setPreference,
+                resetPreference,
+                resetPreferences,
+                setPreferences: setContextPreferences,
+            }}
+        >
+            {children}
+        </PreferencesContext.Provider>
+    );
 }
